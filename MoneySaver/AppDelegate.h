5573b39068a20fd5b0d25703b58d7cd3858a0939
TBXark <vfanx@qq.com>
2015-09-21 09:20:17 +0800

    Initial Commit

---
+++ MoneySaver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoneySaver
+//
+//  Created by Tbxark on 15/9/21.
+//  Copyright © 2015年 TBXark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
